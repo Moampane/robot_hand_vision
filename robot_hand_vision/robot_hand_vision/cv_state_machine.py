21c9d79f4d57480481892ebf8a1d6ec8e49a19c3
dfriishecht <dfriishecht@olin.edu>
2023-10-26 17:50:04 -0400

    Create camera subscription

--- robot_hand_vision/robot_hand_vision/cv_state_machine.py
+++ robot_hand_vision/robot_hand_vision/cv_state_machine.py
@@ -1,6 +1,28 @@
+import rclpy
+from rclpy.node import Node
+from math import acos, sqrt
+import numpy as np
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+
+class FiniteStateController(Node):
+    def __init__(self, image_topic):
+        super().__init__("finite_state_controller")
+        self.camera_sub = self.create_subscription(
+            Image, image_topic, self.process_image, 10
+        )
+
+
 def main():
-    print('Hi from robot_hand_vision.')
+    fsm_publisher = FiniteStateController()
+    rclpy.init()
+    rclpy.spin(fsm_publisher)
+    fsm_publisher.destroy_node()
+    rclpy.shutdown()
+    print("Hi from robot_hand_vision.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
